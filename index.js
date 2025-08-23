7fdeea0ef7ee4ef231b667baf90f0fe756808413
Arshh26 <arshadali99936@gmail.com>
2025-08-23 18:48:01 +0530

    Added Functions

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let itemsContainerElement = document.querySelector('.items-container');
+displayItemsOnHomePage();
+function addToBag() {
+
+}
+
+function displayItemsOnHomePage () {
+  let itemsContainerElement = document.querySelector('.items-container');
 
 let innerHtml = '';
 items.forEach(item => {
@@ -13,9 +19,32 @@ items.forEach(item => {
            <span class="original-price">Rs ${item.original_price}</span>
            <span class="discount">(${item.discount_percentage}% OFF)</span>
      </div>
- <button class="btn-add-bag">Add to Bag</button>
+ <button class="btn-add-bag" onclick="addToBag()">Add to Bag</button>
 </div>`
 });
 
 itemsContainerElement.innerHTML = innerHtml;
+
+}
+
+// let itemsContainerElement = document.querySelector('.items-container');
+
+// let innerHtml = '';
+// items.forEach(item => {
+//   innerHtml += `
+//   <div class="item-container">
+//     <img class="item-image" src="${item.item_image}" alt="item image">
+//      <div class="rating">${item.rating.stars}⭐ | ${item.rating.noOfReviews} </div>
+//         <div class="company-name">${item.company_Name}</div>
+//         <div class="item-name">${item.item_name}</div>
+//         <div class="price">
+//            <span class="current-price">Rs ${item.current_price}</span>
+//            <span class="original-price">Rs ${item.original_price}</span>
+//            <span class="discount">(${item.discount_percentage}% OFF)</span>
+//      </div>
+//  <button class="btn-add-bag" onclick="addToBag()">Add to Bag</button>
+// </div>`
+// });
+
+// itemsContainerElement.innerHTML = innerHtml;
 
